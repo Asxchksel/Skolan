c79d7c78bada05074a6b24e57ed55278fac970ff
axel.mannheimer <axel.mannheimer@5CD132CZP9.learnet.se>
2023-01-12 15:22:07 +0100

    uppdateing

--- src/slagsmål_projekt/Slagsmål_projekt.java
+++ src/slagsmål_projekt/Slagsmål_projekt.java
@@ -1,9 +1,12 @@
 package slagsmål_projekt;
 
 import java.util.Scanner;
-// Tststata
+
 public class Slagsmål_projekt {
 
+    public static short Php = 100;
+    public static short Chp = 150;
+    
     public static void main(String[] args) {
         Scanner keyb = new Scanner(System.in);
         System.out.println("F(fight) eller S(shop): ");
@@ -17,8 +20,6 @@ public class Slagsmål_projekt {
     }
 
     static void Fight() {
-        short Chp = 150;
-        short Php = 150;
 
         Scanner keyb = new Scanner(System.in);
 
@@ -33,8 +34,10 @@ public class Slagsmål_projekt {
                     System.out.println("Din attack träffade, du gör 10 damage");
                     Chp = (short) (Chp - 10);
                     System.out.println("Fiende har " + Chp + " Hp kvar");
+                    Fiende();
                 } else {
                     System.out.println("Din attack missade");
+                    Fiende();
                 }
 
             }
@@ -44,26 +47,47 @@ public class Slagsmål_projekt {
                     System.out.println("Din attack träffade, du gör 40 damage");
                     Chp = (short) (Chp - 40);
                     System.out.println("Fiende har " + Chp + " Hp kvar");
+                    Fiende();
                 } else {
                     System.out.println("Din attack missade");
+                    Fiende();
+                }
+            }
+            
+            else if(att == 3) {
+                double evade = Math.random() * 100;
+                if (evade >= 50){
+                    System.out.println("Fiend attack missade");
                 }
             }
         }
     }
 
+    static void Fiende(){
+        double fattack = Math.random() * 100;
+        if (fattack > 33){
+            System.out.println("Din fiendes attack träffade, dem gör 25 skada. Du har " + Php +" hp kvar.");
+        }
+        else{
+            System.out.println("Din fiendes attack missade, du hade tur.");
+    }
+    }
+    
     static void Shop() {
         Scanner keyb = new Scanner(System.in);
         String[] fält = new String[4];
         fält[0] = "Redbull - 50 Vbucks";
         fält[1] = "Vatten - 30 Vbucks";
-        fält[2] = "C";
-        fält[3] = "D";
+        fält[2] = "Doritos - 75 Vbucks";
+        fält[3] = "d";
         
          for(int i=0; i<fält.length; i++)
         {
-            System.out.println("Objekt " + i + ": "+fält[i]);
+            System.out.println(i + ": " + fält[i]);
         }
 
         System.out.println("Vad vill du köpa?: ");
+        int köp = keyb.nextInt();
+        
     }
 }
