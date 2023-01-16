98c5bb20abd91fc1267d9e18af35f96cc708bfd4
axel.mannheimer <axel.mannheimer@5CD132CZP9.learnet.se>
2023-01-16 13:30:10 +0100

    uppdateing

--- src/slagsmål_projekt/Slagsmål_projekt.java
+++ src/slagsmål_projekt/Slagsmål_projekt.java
@@ -16,14 +16,15 @@ public class Slagsmål_projekt {
         } else if (fs.equalsIgnoreCase("s")) {
             Shop();
         }
+        System.out.println("Vill du spela igen?");
 
     }
 
     static void Fight() {
         while (Chp > 0 && Php > 0) {
-
+            int fattack = (int) (Math.random() * 100);
             System.out.println("Vilken Attack vill du använda: ");
-            int att = keyb.nextInt();
+            byte att = keyb.nextByte();
 
             if (att == 1) {
                 int träff = (int) (Math.random() * 100);
@@ -31,10 +32,10 @@ public class Slagsmål_projekt {
                     System.out.println("Din attack träffade, du gör 10 damage");
                     Chp = (short) (Chp - 10);
                     System.out.println("Fiende har " + Chp + " Hp kvar");
-                    Fiende();
+                    Fiende(fattack);
                 } else {
                     System.out.println("Din attack missade");
-                    Fiende();
+                    Fiende(fattack);
                 }
 
             }
@@ -44,32 +45,30 @@ public class Slagsmål_projekt {
                     System.out.println("Din attack träffade, du gör 40 damage");
                     Chp = (short) (Chp - 40);
                     System.out.println("Fiende har " + Chp + " Hp kvar");
-                    Fiende();
+                    Fiende(fattack);
                 } else {
                     System.out.println("Din attack missade");
-                    Fiende();
+                    Fiende(fattack);
                 }
             }
             
             else if(att == 3) {
                 double evade = (Math.random() * 100);
                 if (evade >= 50){
-                   /* System.out.println(fattack);
-                    fattack = fattack - 15;
-                    System.out.println(fattack);*/
-                    Fiende();
+                    System.out.println("Du försöker undvika fiende attacken och sänker deras träffsäkerhet");
+                    fattack = fattack - 10;
+                    Fiende(fattack);
                 }
                 else{
                     System.out.println("Du nuddade gräss ofriviligt");
-                    Fiende();
+                    Fiende(fattack);
                 }
             }
         }
     }
 
-    static void Fiende(){
-        int fattack = (int) (Math.random() * 100);
-        if (fattack > 45){
+    static void Fiende(int fattack){ 
+        if (fattack > 40){
             Php =(short) (Php - 25);
             System.out.println("Din fiendes attack träffade, dem gör 25 skada. Du har " + Php +" hp kvar.");
         }
@@ -79,11 +78,12 @@ public class Slagsmål_projekt {
     }
     
     static void Shop() {
+        int vbucks = 600;
         String[] fält = new String[4];
         fält[0] = "Redbull - 50 Vbucks";
         fält[1] = "Vatten - 30 Vbucks";
         fält[2] = "Doritos - 75 Vbucks";
-        fält[3] = "d";
+        fält[3] = "Book - 600 Vbucks";
         
          for(int i=0; i<fält.length; i++)
         {
@@ -93,7 +93,7 @@ public class Slagsmål_projekt {
         System.out.println("Vad vill du köpa?: ");
         int köp = keyb.nextInt();
         
-        ;
+        
         
     }
 }
